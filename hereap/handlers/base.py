7cc5237f8801bcf67efe61a5c75d7f385bf01ea8
thislight <thislight@noreply.users.github.com>
2016-11-07 22:22:26 +0800

    Update BaseHandler, config and get_current_user.

--- hereap/handlers/base.py
+++ hereap/handlers/base.py
@@ -3,21 +3,50 @@
 Base Handler
 """
 
+
 from logging import getLogger
 import methods.db as database
+from .. import config as _c
+import methods.tokenhelper as token
 from tornado.web import RequestHandler
 
 class BaseHandler(RequestHandler):
     @property
     def database(self):
+        """
+        () -> pymongo.MongoClient
+        
+        Return a mongodb database client from configs.
+        """
         return database.client()
 
     def get_current_user(self):
-        user_cookie = self.get_secure_cookie('user')
+        """
+        () -> dict
+        
+        Get current user, return a dict for from token
+        """
+        user_cookie = self.get_secure_cookie('user_token')
         if user_cookie:
-            return user_cookie
+            user=token.find_token(user_cookie)
+            if user: return user
         return None
 
     @property
     def logger(self):
+        """
+        () -> logging.Logger
+        
+        Get logger
+        """
         return getLogger(__name__)
+    
+    @property
+    def _c(self):
+        """
+        () -> ..config
+        
+        Get configs.
+        """
+        return _c
+
