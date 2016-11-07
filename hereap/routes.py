157943628e3c1ace33a46414b968a6a24f8bd129
thislight <thislight@noreply.users.github.com>
2016-11-07 22:23:43 +0800

    Add route function.

--- hereap/routes.py
+++ hereap/routes.py
@@ -3,13 +3,25 @@
 Routes for website
 """
 from config import app_configs as _app_c
-from handlers.devs.status_code_handler import Return200Handler
 
 routes=[]
 
+from handlers.devs.status_code_handler import Return200Handler
 
 _dev_routes=[(r'/dev/r200',Return200Handler)]
 
 
+def route(c,url,**kargs):
+    if len(kargs) > 0:
+        routes.append((url,c))
+    else:
+        routes.append((url,c,kargs))
+    return c
+
+
+
+
+
 if _app_c['debug']:
     routes[len(routes):] = _dev_routes[:]
+
